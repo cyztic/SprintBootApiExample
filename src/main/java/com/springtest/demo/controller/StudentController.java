085f4f5166765449a9e658550175afed22785dae
Josh Wheeler <joshwheeler@Joshs-MacBook-Pro.local>
2019-05-28 12:17:23 -0500

    added delete and update methods

--- src/main/java/com/springtest/demo/controller/StudentController.java
+++ src/main/java/com/springtest/demo/controller/StudentController.java
@@ -3,11 +3,9 @@ package com.springtest.demo.controller;
 import com.springtest.demo.model.Student;
 import com.springtest.demo.service.StudentService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.Media;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,11 +28,38 @@ public class StudentController {
     }
 
     @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            path = "{studentId}"
+    )
+    public Student getStudentById(@PathVariable("studentId") UUID studentId) {
+        return studentService.getstudentById(studentId);
+    }
+
+    @RequestMapping(
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
     public void insertNewStudent(@RequestBody Student student) {
         studentService.persistNewStudent(UUID.randomUUID(), student);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            path = "{studentId}"
+    )
+    public void updateStudent(@PathVariable("studentId") UUID studentId, @RequestBody Student student) {
+        studentService.updateStudentById(studentId, student);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            path = "{studentId}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void deleteStudent(@PathVariable("studentId") UUID studentId) {
+        studentService.deleteStudentById(studentId);
     }
 
 }
